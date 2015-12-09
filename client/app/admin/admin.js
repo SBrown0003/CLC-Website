1cee91c7ac71f29bdfe4c0ec2c0b3d8383f463da
SBrown0003 <stephen.brown@careerbuilder.com>
2015-12-09 18:10:32 -0500

    initializing starter project

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('clcWebsiteApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
