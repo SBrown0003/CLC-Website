783e451f6ea17371bd7c534242a8fe6ef3d11651
SBrown0003 <stephen.brown@careerbuilder.com>
2015-12-09 18:52:09 -0500

    Changing DB schemas and adding service API endpoint.

--- server/api/event/event.model.js
+++ server/api/event/event.model.js
@@ -4,11 +4,12 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var EventSchema = new Schema({
+  id: Number,
   name: String,
   info: String,
   active: Boolean,
-  date: String,
-  location: string
+  date: Date,
+  location: String
 });
 
 module.exports = mongoose.model('Event', EventSchema);
